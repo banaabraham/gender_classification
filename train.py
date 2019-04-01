faf6b344024772cedbde7822cfcc7c03bc0b5d1e
Julisa Bana Abraham <bana.abraham@gmail.com>
2019-04-01 13:44:23 +0700

    Create train.py

---
+++ train.py
@@ -0,0 +1,81 @@
+import matplotlib.patches as patches
+import cv2
+from face_detect import *
+import numpy as np
+import matplotlib.pyplot as plt
+import pandas
+from keras import layers
+from keras import models
+from keras.optimizers import RMSprop
+from keras.preprocessing.image import ImageDataGenerator, array_to_img, img_to_array, load_img
+import scipy
+import os
+import warnings
+warnings.filterwarnings("ignore", category=DeprecationWarning) 
+
+
+
+direktori = "directory that contains man and woman folder"
+
+
+def imageToArrayData(Dir,c_code):
+    label = []
+    imageArray = []
+    train_names = []
+    for file in os.listdir(Dir):
+        if file.endswith(".jpg"):
+            train_names.append(file)
+    for name in train_names:
+        try:
+            img = crop_face(Dir+"\\"+name)
+            y = cv2.resize(img,(150,150)) 
+            y = y /255
+            #img = load_img(Dir+"\\"+name)
+            #y = scipy.misc.imresize(img_to_array(img), new_shape) / 255
+            label.append(c_code)
+            imageArray.append(y)
+        except:
+            pass
+    return np.array(imageArray),label
+
+label = os.listdir(direktori)
+
+train = []
+target = []
+
+for i,l in enumerate(label):
+    tempdir = direktori+"\\"+l
+    tempres = imageToArrayData(tempdir,i)
+    train.append(tempres[0])
+    target.append(tempres[1])
+
+train = np.vstack(train)
+target = np.hstack(target)
+
+"""
+4 layer CNN
+"""
+
+model = models.Sequential()
+model.add(layers.Conv2D(32, (3, 3), activation='relu', input_shape=(150, 150, 3)))
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+model.add(layers.MaxPooling2D((2, 2)))
+model.add(layers.Conv2D(64, (3, 3), activation='relu'))
+
+
+model.add(layers.Flatten())
+model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(1,activation="sigmoid"))
+
+model.compile(loss="binary_crossentropy",optimizer="adam")
+model.fit(train,target,epochs=50,batch_size=10,validation_split=2)
+
+model.save("gender_class.h5")
