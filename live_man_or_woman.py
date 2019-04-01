ec5cd62aa72bcc0b6483205f7d6333c8252f66f0
Julisa Bana Abraham <bana.abraham@gmail.com>
2019-04-01 13:51:26 +0700

    Update live_man_or_woman.py

--- live_man_or_woman.py
+++ live_man_or_woman.py
@@ -12,24 +12,21 @@ model = models.load_model("gender_classs.h5")
 faceCascade = cv2.CascadeClassifier("haarcascade_frontalface_default.xml")
 
 while(True):
-    
-	# Capture frame-by-frame
+	
     ret,frame = cap.read()
     
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    
-    # Detect faces in the image
+
     faces = faceCascade.detectMultiScale(
 		gray,
 		scaleFactor=1.1,
 		minNeighbors=5,
 		minSize=(30, 30)
-		#flags = cv2.CV_HAAR_SCALE_IMAGE
         )
     
-
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+	
     try:
         c = frame[y:y+h, x:x+w]
         c = cv2.resize(c,(150,150))
@@ -41,11 +38,12 @@ while(True):
             cv2.putText(frame,"MAN",(x,y),font,0.5,(0,0,255),2,cv2.LINE_AA)
     except:
         continue
-	# Display the resulting frame
+	
+	
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
-# When everything done, release the capture
+
 cap.release()
 cv2.destroyAllWindows()
