e77300bb463c83283a801e45578d2e2947e9912c
Julisa Bana Abraham <bana.abraham@gmail.com>
2019-04-02 18:43:56 +0700

    Update train.py

--- train.py
+++ train.py
@@ -56,17 +56,10 @@ model.add(layers.MaxPooling2D((2, 2)))
 model.add(layers.Conv2D(64, (3, 3), activation='relu'))
 
 model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
-model.add(layers.MaxPooling2D((2, 2)))
-model.add(layers.Conv2D(64, (3, 3), activation='relu'))
-
+model.add(layers.Conv2D(128, (3, 3), activation='relu'))
 
 model.add(layers.Flatten())
-model.add(layers.Dense(256, activation='relu'))
+model.add(layers.Dense(128, activation='relu'))
 model.add(layers.Dense(1,activation="sigmoid"))
 
 model.compile(loss="binary_crossentropy",optimizer="adam")
